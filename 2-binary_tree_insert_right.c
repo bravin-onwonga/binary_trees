5ee8227c6528b5489ed04e35ebca6977886cf372
bravin-onwonga <onbravin@gmail.com>
2024-03-27 22:03:21 +0300

    Task:Insert right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a child node to the right
+ *
+ * @parent: parent node
+ * @value: value of child node
+ *
+ * Return: Created node; otherwise NULL
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->n = value;
+	node->left = NULL;
+	node->parent = parent;
+	node->right = parent->right;
+
+	if (node->right)
+		node->right->parent = node;
+
+	parent->right = node;
+
+	return (node);
+}
